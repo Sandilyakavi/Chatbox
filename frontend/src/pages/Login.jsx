6f652d9754f674780e8c22b2717ad620e3748bae
Sandilyakavi <sandilyakavi@gmail.com>
2026-06-13 00:52:40 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,9 @@ export default function Login() {
     e.preventDefault();
     setError(''); setLoading(true);
     try {
-      const res = await fetch('http://localhost:5001/api/auth/login', {
+      const API_URL = import.meta.env.VITE_API_URL;
+
+const res = await fetch(`${API_URL}/api/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
